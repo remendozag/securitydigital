8b84f3078b794b16591426e3200cd59d8d21dd41
remendozag <remendozag@gmail.com>
2019-07-01 19:34:52 -0500

    Primera Version

--- security_app/lib/BackSecurity.dart
+++ security_app/lib/BackSecurity.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/material.dart'
 
 class BackSecurity extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        new GradientBack(),
+        new Positioned(
+          bottom: 0.0,
+          child: new Container(
+            width:MediaQuery.of(context).size.width,
+            height:125.0,
+            color: Colors.white,
+          )
+        )
+      ]
+    )
+  }
+}
 
+class GradientBack extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          colors: [
+            Color(0xFFffbb00),
+            Color(0xFFf38c02)
+          ]
+        )
+      )
+    )
+  }
 }
